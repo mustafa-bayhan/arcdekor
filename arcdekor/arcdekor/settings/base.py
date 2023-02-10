aa035b595742236e6ddf527bff4a221ef64ebd05
mustafa-bayhan <bayhanmu19@itu.edu.tr>
2023-02-10 16:17:44 +0000

    from server 20230210 1917

--- arcdekor/arcdekor/settings/base.py
+++ arcdekor/arcdekor/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -117,7 +117,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL ='/media/'
 MEDIA_ROOT =os.path.join(BASE_DIR,'media')
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
